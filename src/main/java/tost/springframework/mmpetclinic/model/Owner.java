d6aea83b8469b630117d31bf236da4fe5ab78ebd
Maurice <maurice.molin@gmail.com>
2020-08-26 22:54:52 -0400

    adding POJO's. Closes #2

---
+++ src/main/java/tost/springframework/mmpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package tost.springframework.mmpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/tost/springframework/mmpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package tost.springframework.mmpetclinic.model;
+
+public class Person {
+
+    private String fistName;
+    private String lastName;
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
