ffcfeae7bd7e2ef55c16eaaebe0d7101558736c4
Maurice <maurice.molin@gmail.com>
2020-08-26 22:24:24 -0400

    Initial Project Add

---
+++ src/main/java/tost/springframework/mmpetclinic/MmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package tost.springframework.mmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tost/springframework/mmpetclinic/MmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package tost.springframework.mmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
