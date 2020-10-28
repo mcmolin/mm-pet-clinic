ed9ed61c44f021b056f50a41724d8d9fc06af43b
Maurice <maurice.molin@gmail.com>
2020-10-28 15:08:55 -0400

    Impl Map base Service, closes #6

---
+++ pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package tost.sprintframework.mmpetclinic.services.map;
+
+import tost.sprintframework.mmpetclinic.model.Pet;
+import tost.sprintframework.mmpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
