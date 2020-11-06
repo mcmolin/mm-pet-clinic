81e7b7df6f81de827ae08fbf4b1187173e50c4e3
Maurice <maurice.molin@gmail.com>
2020-11-06 07:22:57 -0500

    Added visits, Updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package tost.sprintframework.mmpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
