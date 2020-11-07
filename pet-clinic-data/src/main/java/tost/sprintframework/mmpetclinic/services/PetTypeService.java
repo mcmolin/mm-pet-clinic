2b0f40179ea6ee16d6e9ac1b6beb710b3643ebb2
Maurice <maurice.molin@gmail.com>
2020-11-07 05:30:45 -0500

    Added PetTypeService Closes#29

---
+++ pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package tost.sprintframework.mmpetclinic.services;
+
+import tost.sprintframework.mmpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
