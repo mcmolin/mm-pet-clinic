fdb100a67adea5a6ce70565e47cb80957983f2ff
Maurice <maurice.molin@gmail.com>
2020-11-14 09:53:33 -0500

    Created PetJpaService. Closes #43

---
+++ pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/services/springdatajpa/PetJpaService.java
@@ -0,0 +1,49 @@
+package tost.sprintframework.mmpetclinic.services.springdatajpa;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import tost.sprintframework.mmpetclinic.model.Pet;
+import tost.sprintframework.mmpetclinic.repositories.PetRepository;
+import tost.sprintframework.mmpetclinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
