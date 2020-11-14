b4e71bdfc48a64ce401425b5501004d1ea52ec65
Maurice <maurice.molin@gmail.com>
2020-11-14 05:53:01 -0500

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/tost/sprintframework/mmpetclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package tost.sprintframework.mmpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -13,5 +20,9 @@ public class BaseEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public boolean isNew(){
+        return this.id == null;
     }
 }
